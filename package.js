9a9c50520930eb1ed813f84d1d62101939ba93bb
Abigail Watson <awatson1978@gmail.com>
2016-11-30 01:14:01 -0600

    updated deps

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-specimen',
-  version: '1.1.1',
+  version: '1.1.2',
   summary: 'HL7 FHIR Resource - Specimen',
   git: 'https://github.com/clinical-meteor/hl7-resource-specimen',
   documentation: 'README.md'
@@ -13,12 +13,12 @@ Package.onUse(function(api) {
   api.use('mongo');
 
   api.use('aldeed:simple-schema@1.3.3');
-  api.use('aldeed:collection2@2.3.3');
+  api.use('aldeed:collection2@2.5.0');
   api.use('simple:json-routes@2.1.0');
   api.use('prime8consulting:meteor-oauth2-server@0.0.2')
 
   api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@0.4.0');
+  api.use('clinical:hl7-resource-datatypes@0.5.6');
 
   api.addFiles('lib/hl7-resource-specimen.js');
   api.addFiles('server/rest.js', 'server');
